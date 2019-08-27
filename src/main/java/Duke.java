cf8db2227a6d888bebdd963464e9f2aaa8a28c9f
Rishab Patwari <e0311251@u.nus.edu>
2019-08-27 23:56:53 +0800

    Add, List Functionality Added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,52 @@
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Duke {
 
-    private static void printReply(String word) {
-        System.out.println("  " + word);
+    private static void addWord(String word, Vector wordList) {
+        wordList.add(word);
+        System.out.println("____________________________________________");
+        System.out.println("  added: "+ word);
         System.out.println("____________________________________________");
     }
+
+    private static void viewList(Vector wordList){
+        System.out.println("____________________________________________");
+        for(int i = 0; i < wordList.size(); ++i){
+            System.out.println((i+1) + ". " + wordList.get(i));
+        }
+        System.out.println("____________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo
                 + "____________________________________________\n"
-                + "  Hello I'm Duke!\n  What can I do for you?\n"
+                + "  Hello I'm Duke!\n"
+                + "  What can I do for you?\n"
                 + "____________________________________________\n");
+
+        Vector wordList = new Vector(100);
         Scanner input = new Scanner(System.in);
+
         String word = input.nextLine();
-        while( !word.equals("bye")){
-            printReply(word);
+        while (!word.equals("bye")) {
+            if (word.equals("list")) {
+                viewList(wordList);
+            }
+            else {
+                addWord(word, wordList);
+            }
             word = input.nextLine();
         }
-        printReply(word);
-        System.exit(0);
+            System.out.println("____________________________________________\n"
+                                + "  Bye. Hope to see you again soon!\n"
+                                + "____________________________________________\n");
+            System.exit(0);
     }
 }
