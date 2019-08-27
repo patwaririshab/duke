d6248fe649f494319307a5d0488933120500a35f
Rishab Patwari <e0311251@u.nus.edu>
2019-08-27 14:05:08 +0800

    Greet,Echo,Exit functionality completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-                + "- - - - - - - - - - - - -\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________");
+        System.out.println("  Hello I'm Duke!\n  What can I do for you?");
+        System.out.println("____________________________________________");
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        while( !word.equals("bye")) {
+            System.out.println("  " + word);
+            System.out.println("____________________________________________");
+            word = input.next();
+        }
+        System.out.println("  " + word);
+        System.exit(0);
     }
 }
