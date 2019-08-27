f534f2d5b286f9e7299755c2e35b9442e00a4d07
Rishab Patwari <e0311251@u.nus.edu>
2019-08-27 17:18:15 +0800

    Greet,Echo,Exit functionality updated

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________");
-        System.out.println("  Hello I'm Duke!\n  What can I do for you?");
-        System.out.println("____________________________________________");
-        Scanner input = new Scanner(System.in);
-        String word = input.next();
-        while( !word.equals("bye")) {
-            System.out.println("  " + word);
-            System.out.println("____________________________________________");
-            word = input.next();
-        }
+
+    private static void printReply(String word) {
         System.out.println("  " + word);
+        System.out.println("____________________________________________");
+    }
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo
+                + "____________________________________________\n"
+                + "  Hello I'm Duke!\n  What can I do for you?\n"
+                + "____________________________________________\n");
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();
+        while( !word.equals("bye")){
+            printReply(word);
+            word = input.nextLine();
+        }
+        printReply(word);
         System.exit(0);
     }
 }
